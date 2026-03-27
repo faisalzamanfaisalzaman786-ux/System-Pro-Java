92f09eca0f7ac0d7ad70d40046fd35f0831addea
Faisal Zaman <faisal@systempro.com>
2026-03-27 02:50:39 +0000

    System Pro: Fresh Professional Structure

---
+++ app/src/main/java/com/systempro/app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.systempro.app;
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
