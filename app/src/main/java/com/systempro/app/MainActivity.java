7f8af3ee84042919ee6e0f8075754dd901d2aa0f
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-03-30 13:26:49 +0500

    Update MainActivity.java

--- app/src/main/java/com/systempro/app/MainActivity.java
+++ app/src/main/java/com/systempro/app/MainActivity.java
@@ -1,55 +1,13 @@
-package com.systempro.app;
+WebSettings s = webView.getSettings();
+s.setJavaScriptEnabled(true);
+s.setDomStorageEnabled(true); // مینیو ڈیٹا سیو کرنے کے لیے
+s.setDatabaseEnabled(true);
+s.setAllowFileAccess(true);
+s.setGeolocationEnabled(true); // لوکیشن پرمیشن پاپ اپ کے لیے
 
-import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.webkit.WebChromeClient;
-import android.webkit.PermissionRequest;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-    private WebView webView;
-
+webView.setWebChromeClient(new WebChromeClient() {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        webView = new WebView(this);
-        setContentView(webView);
-
-        WebSettings settings = webView.getSettings();
-        
-        // 1. انٹرنیٹ اور جاوا اسکرپٹ کو فعال کرنا
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true); // مینو اور ڈیٹا لوڈنگ کے لیے لازمی
-        settings.setDatabaseEnabled(true);
-        
-        // 2. نیٹ ورک اور مینو بٹنز کی سپورٹ
-        settings.setAllowFileAccess(true);
-        settings.setAllowContentAccess(true);
-        settings.setJavaScriptCanOpenWindowsAutomatically(true);
-        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW); // تاکہ نیٹ سے ڈیٹا آ سکے
-
-        // 3. کلائنٹس سیٹ کرنا (تاکہ لنکس اور مینو ایپ کے اندر کھلیں)
-        webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public void onPermissionRequest(PermissionRequest request) {
-                request.grant(request.getResources());
-            }
-        });
-
-        // 4. فائل لوڈ کرنا
-        webView.loadUrl("file:///android_asset/index.html");
+    public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+        callback.invoke(origin, true, false); // لوکیشن ڈائریکٹ الاؤ کریں
     }
-
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
-    }
-}
+});
