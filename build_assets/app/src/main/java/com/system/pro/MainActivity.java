72df4cb406c42d8d42a9594c6db3c3db1655bfab
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-05-21 10:17:55 +0500

    Update build_assets/app/src/main/java/com/system/pro/MainActivity.java

---
+++ build_assets/app/src/main/java/com/system/pro/MainActivity.java
@@ -0,0 +1,13 @@
+package com.system.pro;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.system.pro.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
