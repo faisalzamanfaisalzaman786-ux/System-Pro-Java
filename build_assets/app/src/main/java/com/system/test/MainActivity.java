687c94c8d84a4eb083848d173985aa76848197ca
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-05-22 17:29:07 +0500

    Update build_assets/app/src/main/java/com/system/test/MainActivity.java

--- build_assets/app/src/main/java/com/system/test/MainActivity.java
+++ build_assets/app/src/main/java/com/system/test/MainActivity.java
@@ -1,1 +1,13 @@
 package com.system.test;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.system.pro.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
