070912291a35b834d88f0cd1d83836f0515462c8
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-05-25 04:58:31 +0500

    System Pro: webview (com.systempro.webview1)

---
+++ app/src/main/java/com/systempro/webview1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.systempro.webview1;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.textView);
+        tv.setText("webview is ready!");
+    }
+}
