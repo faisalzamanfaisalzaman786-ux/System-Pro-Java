ec53e52facdc589ae1d7ee88fa096b050ba6e59e
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-05-24 18:22:13 +0500

    Add/update app/src/main/java/com/systempro/webview/MainActivity.java

---
+++ app/src/main/java/com/systempro/webview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.systempro.webview;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.textView);
+        tv.setText("Webview is ready!");
+    }
+}
