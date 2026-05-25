bdb2235c2b35275b3fac26cb3860f052daa6f8b5
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-05-25 09:04:08 +0500

    System Pro: helloworld (com.systempro.helloworld) - atomic clean replace

---
+++ app/src/main/java/com/systempro/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package com.systempro.helloworld;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com");
+        setContentView(webView);
+    }
+}
