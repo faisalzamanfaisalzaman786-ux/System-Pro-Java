1f207d7de6eeab97b4d3ce1822ef33377b0d81e0
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-05-28 20:36:48 +0500

    Update MainActivity.java

--- app/src/main/java/com/systempro/app/MainActivity.java
+++ app/src/main/java/com/systempro/app/MainActivity.java
@@ -1,7 +1,11 @@
 package com.systempro.app;
-import android.app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-public class MainActivity extends Activity {
-    @Override protected void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); setContentView(R.layout.activity_main); TextView tv = findViewById(R.id.textView); tv.setText("CodeForge Pro Works!"); }
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
