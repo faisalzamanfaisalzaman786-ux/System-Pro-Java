ddb547f006d40af607e781cb2978992c40b82c1d
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-06-07 05:45:43 +0500

    Update app/src/main/java/com/systempro/app/MainActivity.java

--- app/src/main/java/com/systempro/app/MainActivity.java
+++ app/src/main/java/com/systempro/app/MainActivity.java
@@ -1,30 +1,12 @@
-package com.example.simpleapp;
+package com.example.colortest;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private int counter = 0;
-    private TextView counterText;
-    private Button clickButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        counterText = findViewById(R.id.counterText);
-        clickButton = findViewById(R.id.clickButton);
-
-        clickButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                counter++;
-                counterText.setText("Button clicked: " + counter + " times");
-            }
-        });
     }
 }
