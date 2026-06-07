df3d70a89cffd8ca38cf74117cddfd4d6962f1ce
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-06-07 05:35:04 +0500

    Update app/src/main/java/com/systempro/app/MainActivity.java

--- app/src/main/java/com/systempro/app/MainActivity.java
+++ app/src/main/java/com/systempro/app/MainActivity.java
@@ -1,34 +1,30 @@
 package com.example.simpleapp;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    
-    private TextView textView;
-    private Button clickButton;
     private int counter = 0;
-    
+    private TextView counterText;
+    private Button clickButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        // Initialize views
-        textView = findViewById(R.id.textView);
+
+        counterText = findViewById(R.id.counterText);
         clickButton = findViewById(R.id.clickButton);
-        
-        // Set click listener on button
-        clickButton.setOnClickListener(view -> {
-            counter++;
-            textView.setText("Button clicked: " + counter + " times");
-            Toast.makeText(MainActivity.this, "Clicked " + counter, Toast.LENGTH_SHORT).show();
+
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                counterText.setText("Button clicked: " + counter + " times");
+            }
         });
-        
-        // Set initial text
-        textView.setText("Welcome to Simple App!");
     }
 }
