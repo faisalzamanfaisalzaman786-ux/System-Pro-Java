76b79e74020ab4d3a345ba852d5caaf66176e412
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-06-08 21:29:35 +0500

    Update app/src/main/java/com/systempro/app5/MainActivity.java

--- app/src/main/java/com/systempro/app5/MainActivity.java
+++ app/src/main/java/com/systempro/app5/MainActivity.java
@@ -1,17 +1,57 @@
-package com.example.helloworld;
+package com.example.counter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private TextView counterTextView;
+    private Button incrementButton;
+    private Button decrementButton;
+    private Button resetButton;
+    private int counter = 0;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        TextView textView = findViewById(R.id.textView);
-        textView.setText("Hello World!");
+        counterTextView = findViewById(R.id.counterTextView);
+        incrementButton = findViewById(R.id.incrementButton);
+        decrementButton = findViewById(R.id.decrementButton);
+        resetButton = findViewById(R.id.resetButton);
+        
+        updateCounterDisplay();
+        
+        incrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                updateCounterDisplay();
+            }
+        });
+        
+        decrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter--;
+                updateCounterDisplay();
+            }
+        });
+        
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter = 0;
+                updateCounterDisplay();
+            }
+        });
+    }
+    
+    private void updateCounterDisplay() {
+        counterTextView.setText(String.valueOf(counter));
     }
 }
