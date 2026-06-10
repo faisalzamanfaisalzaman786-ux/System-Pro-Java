07f7ce9179db49007ee8bc0216ee3affd1bbc681
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-06-10 17:58:27 +0500

    Update MainActivity.java

--- build_assets/MainActivity.java
+++ build_assets/MainActivity.java
@@ -1,1 +1,47 @@
+package com.counter.app;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView counterTextView;
+    private Button incrementButton;
+    private Button decrementButton;
+    private Button resetButton;
+    private int counter = 0;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        counterTextView = findViewById(R.id.counterTextView);
+        incrementButton = findViewById(R.id.incrementButton);
+        decrementButton = findViewById(R.id.decrementButton);
+        resetButton = findViewById(R.id.resetButton);
+        
+        updateCounterDisplay();
+        
+        incrementButton.setOnClickListener(v -> {
+            counter++;
+            updateCounterDisplay();
+        });
+        
+        decrementButton.setOnClickListener(v -> {
+            counter--;
+            updateCounterDisplay();
+        });
+        
+        resetButton.setOnClickListener(v -> {
+            counter = 0;
+            updateCounterDisplay();
+        });
+    }
+    
+    private void updateCounterDisplay() {
+        counterTextView.setText(String.valueOf(counter));
+    }
+}
