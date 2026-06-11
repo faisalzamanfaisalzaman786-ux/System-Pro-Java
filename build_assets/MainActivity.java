8f9122af06d68226c5d5ef3c7c09f70f3b3604d0
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-06-11 08:15:50 +0500

    Update build_assets/MainActivity.java

--- build_assets/MainActivity.java
+++ build_assets/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean isNewInput = true;
 
     @Override
-    protected void bundleState(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
